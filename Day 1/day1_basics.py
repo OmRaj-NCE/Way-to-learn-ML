8c278d62bb93d34f0b56c23b8ead425c3cdd0c61
Om Raj <omraj0503@gmail.com>
2025-12-22 18:44:53 +0530

    Create day1_basics.py

---
+++ Day 1/day1_basics.py
@@ -0,0 +1,14 @@
+print("Hello, ML World!")
+print("I am starting my journey to become an ML Engineer.")
+
+# To run : python day1_basics.py
+
+# 🌱 PART 5: Very First Concepts (Light but Important)
+# 1️⃣ Comments
+# This is a comment (Python ignores it)
+# 2️⃣ Variables (Just intuition today)
+age = 18
+name = "Raj"
+print(age)
+print(name)
+# Variable = container for data.
