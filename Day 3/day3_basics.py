ff8ff3e6949b6e9aa8ba246beab52a9c89f5e33c
Om Raj <omraj0503@gmail.com>
2025-12-22 18:55:43 +0530

    Create day3_basics.py

---
+++ Day 3/day3_basics.py
@@ -0,0 +1,34 @@
+# 🔍 PART 2: Comparison Operators (MEMORIZE)
+# Operator	Meaning
+# >	        greater than
+# <	        less than
+# >=	    greater or equal
+# <=	    less or equal
+# ==	    equal to
+# !=	    not equal
+print(10 > 5)   # True
+print(10 == 5)  # False
+
+
+# 🧱 PART 3: if Statement (Core Syntax)
+age = 18
+if age >= 18:
+    print("You are eligible to vote")
+
+# 🔁 PART 4: if – else
+age = 16
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+# 🔀 PART 5: elif (Multiple Conditions)
+marks = 72
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Fail")
