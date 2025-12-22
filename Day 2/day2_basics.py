5bb1df562ab8a9a8f0e2b3d1a68a670cc0d55a60
Om Raj <omraj0503@gmail.com>
2025-12-22 18:50:15 +0530

    Create day2_basics.py

---
+++ Day 2/day2_basics.py
@@ -0,0 +1,36 @@
+# 📦 PART 2: Core Python Data Types (VERY IMPORTANT)
+# 1️⃣ Integer (int)
+# Whole numbers
+# age = 18
+# marks = 95
+# 2️⃣ Float (float)
+# Decimal numbers
+# height = 167.5
+# price = 99.99
+# 3️⃣ String (str)
+# Text (always inside quotes)
+# name = "Raj"
+# college = "NCE Chandi"
+# 4️⃣ Boolean (bool)
+# True or False
+# is_student = True
+
+#🧪 PART 3: Checking Data Type
+x = 10
+print(type(x))
+
+# ⌨️ PART 4: Taking Input from User
+name = input("Enter your name: ")
+print(name)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a + b)
+
+# 🧮 PART 6: Operators (Just What You Need Today)
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
