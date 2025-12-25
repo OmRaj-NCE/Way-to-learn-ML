f11899312590c352898778bae7751f6bd1e6f413
Om Raj <omraj0503@gmail.com>
2025-12-25 13:02:27 +0530

    Create day16_student_report.py

---
+++ Week 3/Day 16/day16_student_report.py
@@ -0,0 +1,10 @@
+def student_report(name, marks, grade="Not Assigned", school="ABC School"):
+    print("\n--- STUDENT REPORT ---")
+    print("Name:", name)
+    print("Marks:", marks)
+    print("Grade:", grade)
+    print("School:", school)
+
+student_report("Raj", 85)
+student_report("Aman", 92, grade="A")
+student_report(name="Sita", marks=78, school="Delhi Public School")
