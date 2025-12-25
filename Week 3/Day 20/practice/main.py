cc317aed61d694e3d30735804a0d4b233c3d195b
Om Raj <omraj0503@gmail.com>
2025-12-25 14:47:34 +0530

    Create main.py

---
+++ Week 3/Day 20/practice/main.py
@@ -0,0 +1,11 @@
+import utils
+
+print("Utility Toolkit")
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Add:", utils.add(x, y))
+print("Subtract:", utils.subtract(x, y))
+print("Multiply:", utils.multiply(x, y))
+print("Divide:", utils.divide(x, y))
