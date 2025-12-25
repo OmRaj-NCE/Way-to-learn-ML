7e464c7f388a8ab13094dd6667d4404b3d242abc
Om Raj <omraj0503@gmail.com>
2025-12-25 13:28:18 +0530

    Update day17_flexible_report.py

--- Week 3/Day 17/day17_flexible_report.py
+++ Week 3/Day 17/day17_flexible_report.py
@@ -26,3 +26,8 @@ report(
     lr=0.01,
     optimizer="adam"
 )
+
+# Why is flexibility critical in real systems?
+# When you build your Cafe App for Patna, you might hardcode things like currency="INR". Two months later, if you want to launch in Nepal:
+# Rigid System: You have to rewrite the database, the payment function, and the UI to accept a new currency.
+# Flexible System: You designed the payment function with **kwargs or a config object. You just pass currency="NPR" and the system adapts.
