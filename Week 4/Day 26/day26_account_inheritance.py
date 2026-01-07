55f5f30a46fcf817bc6fed6ce2c9b8c40d9b1779
Om Raj <omraj0503@gmail.com>
2026-01-08 00:53:54 +0530

    Update day26_account_inheritance.py

--- Week 4/Day 26/day26_account_inheritance.py
+++ Week 4/Day 26/day26_account_inheritance.py
@@ -7,7 +7,7 @@ class Account:
         self.balance += amount
 
     def show_balance(self):
-        print("Balance:", self.balance)
+        print(f"Owner: {self.owner} | Balance: {self.balance}")
 
 
 class SavingsAccount(Account):
@@ -25,3 +25,10 @@ acc = SavingsAccount("Raj", 1000, 5)
 acc.show_balance()
 acc.add_interest()
 acc.show_balance()
+
+
+# Feature        Defined In,             Accessible By
+# owner          Parent (Account),       Both Parent & Child
+# balance        Parent (Account),       Both Parent & Child
+# rate           Child (SavingsAccount), Only Child
+# add_interest() Child (SavingsAccount), Only Child
